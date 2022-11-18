8c381bfa709478417f63f98b1a3d3368cb6ab8bc
ShashwatSinghRaghav <shashwatraghav02@gmail.com>
2022-11-18 10:11:51 +0530

    time fix

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,26 +1,27 @@
 jQuery(document).ready(function () {
   $(function () {
-    $("#defaultCountdown").countdown({ until: new Date(2022, 12, 23, 11) }); // year, month, date, hour
+    $("#defaultCountdown").countdown({ until: new Date(2022, 11, 23, 11) }); // year, month, date, hour
   });
 });
 
-
 /* Owl Carousel */
-$('.owl-carousel').owlCarousel({
+$(".owl-carousel").owlCarousel({
   center: true,
-    loop:true,
-    margin:0,
-    nav:true,
-    items: 3,
-    smartSpeed: 750,
-    navText: ['<span class="fa fa-angle-left"></span>', '<span class="fa fa-angle-right"></span>'],
-    responsive : {
-      0 : {
-        items: 1
-      },
-      641 : {
-        items: 3
-      }
-  }
-
+  loop: true,
+  margin: 0,
+  nav: true,
+  items: 3,
+  smartSpeed: 750,
+  navText: [
+    '<span class="fa fa-angle-left"></span>',
+    '<span class="fa fa-angle-right"></span>',
+  ],
+  responsive: {
+    0: {
+      items: 1,
+    },
+    641: {
+      items: 3,
+    },
+  },
 });
