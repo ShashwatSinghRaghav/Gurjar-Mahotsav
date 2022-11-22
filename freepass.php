dc2e8882eb9961c753df33a27946df68c43be566
Shashwat <shashwatraghav02@gmail.com>
2022-11-22 18:59:19 +0530

    free pass completed

--- freepass.php
+++ freepass.php
@@ -5,20 +5,50 @@
 $con = mysqli_connect('localhost', 'root', '','freepass');
 
 // get the post records
-$name = $_POST['name'];
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
 
 
 // database insert SQL code
-$sql = "INSERT INTO `user` (`Name`, `Email`, `Phone`) VALUES ('$name', '$email', '$phone')";
+
+$sql = "INSERT INTO `user` (`fname`,`lname`, `email`, `phone`) VALUES ('$fname','$lname', '$email', '$phone')";
 
 // insert in database 
 $rs = mysqli_query($con, $sql);
 
-if($rs)
-{
-	header('Location: /index.html');
+$idselect = "SELECT id from `user` WHERE `fname` = '$fname' and `lname` = '$lname' and `email` = '$email'";
+
+$result = mysqli_query($con, $idselect);
+
+$temp = 0;
+
+while ($row = $result->fetch_assoc()) {
+     $temp = "ID  : GM2022-".$row['id'];
 }
+// database update SQL code
+header('content-type: image/jpeg');
+$font="CALIBRIB.TTF";
+$image = imagecreatefromjpeg("entrypass.jpg");
+$color = imagecolorallocate($image, 255,255,255);
+
+$gate="GATE  : 4A";
+$gate_row="ROW  : 02";
+
+imagettftext($image,30,0,1020,250,$color,$font,$gate);
+imagettftext($image,30,0,1020,300,$color,$font,$gate_row);
+imagettftext($image,20,0,1015,350,$color,$font,$temp);
+
+// This will tell the browser to download it
+header('Content-Disposition: attachment; filename=FreePass.jpg'); 
+
+imagedestroy($image);
+
+// if($rs)
+// {
+// 	header('Location: https://www.gurjarmahotsav.com/');
+// 	exit;
+// }
 
 ?>
